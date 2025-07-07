c0ae2ea1b2c8213cf069cdd44e960b546a426c91
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-07-07 18:58:41 -0300

    Implementação de ClienteRepository e métodos CRUD
    Adicionados namespaces necessários para suporte às funcionalidades.
    Criada a classe ClienteRepository para gerenciar operações de
    banco de dados relacionadas à entidade Cliente. Implementados
    os métodos assíncronos AddAsync, GetFirstAsync, GetWhereAsync,
    RemoveAsync e UpdateAsync, cobrindo operações de criação,
    leitura, atualização e remoção. O método UpdateAsync também
    gerencia a exclusão de endereços não associados ao cliente.

---
+++ ContainerRS/src/ContainerRS.Api/Data/Repositories/ClienteRepository.cs
@@ -0,0 +1,57 @@
+﻿using ContainerRS.Api.Contracts;
+using ContainerRS.Api.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ContainerRS.Api.Data.Repositories
+{
+    public class ClienteRepository(AppDbContext dbContext) : IRepository<Cliente>
+    {
+        public async Task<Cliente> AddAsync(Cliente obj, CancellationToken cancellationToken = default)
+        {
+            await dbContext.Clientes.AddAsync(obj, cancellationToken);
+            await dbContext.SaveChangesAsync(cancellationToken);
+            return obj;
+        }
+
+        public async Task<Cliente?> GetFirstAsync<TProperty>(Expression<Func<Cliente, bool>> predicate, Expression<Func<Cliente, TProperty>> orderBy, CancellationToken cancellationToken = default)
+        {
+            return await dbContext.Clientes
+                .Include(c => c.Enderecos)
+                .AsNoTracking()
+                .OrderBy(orderBy)
+                .FirstOrDefaultAsync(predicate,cancellationToken);
+        }
+
+        public async Task<IEnumerable<Cliente>> GetWhereAsync(Expression<Func<Cliente, bool>>? filtro = null, CancellationToken cancellationToken = default)
+        {
+            IQueryable<Cliente> queryClientes = dbContext.Clientes;
+            if (filtro != null)
+            {
+                queryClientes = queryClientes.Where(filtro);
+            }
+
+            return await queryClientes
+                .AsNoTracking()
+                .ToListAsync(cancellationToken);
+        }
+
+        public async Task RemoveAsync(Cliente obj, CancellationToken cancellationToken = default)
+        {
+            dbContext.Clientes.Remove(obj);
+            await dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task<Cliente> UpdateAsync(Cliente obj, CancellationToken cancellationToken = default)
+        {
+            dbContext.Set<Endereco>()
+                .Where(e => e.ClienteId == obj.Id && !obj.Enderecos.Contains(e))
+                .ToList()
+                .ForEach(e => dbContext.Set<Endereco>().Remove(e));
+
+            dbContext.Clientes.Update(obj);
+            await dbContext.SaveChangesAsync(cancellationToken);
+            return obj;
+        }
+    }
+}
