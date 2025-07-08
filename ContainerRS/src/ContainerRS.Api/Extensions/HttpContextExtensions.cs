6a9ad5372ff64cc442a42e6d84166d6eae3aba9f
Alan Vieira Martins <alanhorcrux@gmail.com>
2025-07-08 15:46:26 -0300

    Adiciona extensão GetClientId para HttpContext
    Adicionada a classe estática `HttpContextExtensions` no namespace
    `ContainerRS.Api.Extensions` com o método de extensão `GetClientId`.
    Esse método utiliza as claims do usuário no contexto HTTP para
    extrair o valor associado ao tipo de claim "ClienteId". Caso o valor
    seja encontrado e válido, ele é convertido para um objeto `Guid` e
    retornado; caso contrário, retorna `null`. A funcionalidade melhora
    a reutilização e legibilidade ao facilitar o acesso ao ID do cliente
    em diferentes partes da aplicação.

---
+++ ContainerRS/src/ContainerRS.Api/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace ContainerRS.Api.Extensions
+{
+    public static class HttpContextExtensions
+    {
+        public static Guid? GetClientId(this HttpContext context)
+        {
+            var clienteId = context.User.Claims
+                .Where(c => c.Type.Equals("ClienteId"))
+                .Select(c => c.Value)
+                .FirstOrDefault();
+
+            return !string.IsNullOrEmpty(clienteId) ? Guid.Parse(clienteId) : null;
+        }
+    }
+}
